7e5fee874ffe3c11b2b849f8a7ff4a106887af97
Victor <vicemanoel@hotmail.com>
2025-03-13 15:28:44 -0300

    repo change

--- internal/adapter/driver/handler/http/customer.go
+++ internal/adapter/driver/handler/http/customer.go
@@ -4,10 +4,10 @@ import (
 	"strconv"
 
 	"github.com/gin-gonic/gin"
-	"github.com/vitovidale/TECH-CHALLENGE/internal/adapter/driver/handler/http/request"
-	"github.com/vitovidale/TECH-CHALLENGE/internal/adapter/driver/handler/http/response"
-	"github.com/vitovidale/TECH-CHALLENGE/internal/core/domain"
-	"github.com/vitovidale/TECH-CHALLENGE/internal/core/port"
+	"github.com/vitovidale/fastfood-app/internal/adapter/driver/handler/http/request"
+	"github.com/vitovidale/fastfood-app/internal/adapter/driver/handler/http/response"
+	"github.com/vitovidale/fastfood-app/internal/core/domain"
+	"github.com/vitovidale/fastfood-app/internal/core/port"
 )
 
 type CustomerHandler struct {
